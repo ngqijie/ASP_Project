56bec2f05c3f65527e9c6da01f666d964cb476b3
ngqijie <ngqijie23@gmail.com>
2021-08-20 09:31:06 +0800

    ASP Project

---
+++ 14_Joston_ASP_Project_Testcase.py
@@ -0,0 +1,14 @@
+import unittest
+import result as prog
+
+class TestMyProgram(unittest.TestCase):
+
+    def test_total(self):
+        data = [12946711, 11363486, 7085418,  5469237, 1180258, 1402976, 1051425, 922636, 180249, 88012, 34626,  24763, 13402, 4029 ]
+        result = prog.AspProject.total(data)
+        self.assertEqual(result,)
+
+    def test_mean(self):
+        data = [12946711, 11363486, 7085418,  5469237, 1180258, 1402976, 1051425, 922636, 180249, 88012, 34626,  24763, 13402, 4029 ]
+        result = prog.AspProject.mean(data)
+        self.assertEqual(result,)
